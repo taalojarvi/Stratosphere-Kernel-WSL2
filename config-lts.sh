09a179b1315929aa62bc142e4ed62c39087d8c8f
taalojarvi <sreedevan05@gmail.com>
2024-06-29 01:10:37 +0530

    config.sh: Move patches to its own subdirectory and recursively apply them
    LTS builds will still legacy script for posterity.
    
    Signed-off-by: taalojarvi <sreedevan05@gmail.com>

--- config-lts.sh
+++ config-lts.sh
@@ -1,10 +1,10 @@
 #
 # Apply dxgkrnl patch
 #
-git apply ../0001-6.1.y-dxgkrnl.patch
-git apply ../0002-dxgkrnl-enable-mainline-support.patch
-git apply ../0003-bbrv3-fix-clang-build.patch
-git apply ../0004-nf_nat_fullcone-fix-clang-uninitialized-var-werror.patch
+git apply ../patches/0001-6.1.y-dxgkrnl.patch
+git apply ../patches/0002-dxgkrnl-enable-mainline-support.patch
+git apply ../patches/0003-bbrv3-fix-clang-build.patch
+git apply ../patches/0004-nf_nat_fullcone-fix-clang-uninitialized-var-werror.patch
 
 if [ $? != 0 ]; then
     echo "Patch conflict!"
@@ -16,4 +16,3 @@ fi
 cp ../wsl2_defconfig ./arch/x86/configs/wsl2_defconfig
 make LLVM=1 LLVM_IAS=1 wsl2_defconfig
 make LLVM=1 LLVM_IAS=1 oldconfig
-
